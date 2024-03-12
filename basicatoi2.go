3f1396607d2b280fe933a413d586f853eabaf1a7
sage-yh <solitarysage.ea@gmail.com>
2024-03-12 17:57:58 +0000

    basicatoi2

---
+++ basicatoi2.go
@@ -0,0 +1,14 @@
+package piscine
+
+func BasicAtoi2(s string) int {
+	res := 0
+	for _, char := range s {
+		if char >= '0' && char <= '9' {
+			res = res*10 + int(char-48)
+		} else {
+			return 0
+		}
+	}
+
+	return res
+}
